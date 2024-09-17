f4df34ef84aa2425a94d9426918c5e1dc073fbbb
TinuPrajapati <tinup2580@gmail.com>
2024-09-17 13:53:09 +0530

    complete

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const authenticateToken = require('./middleware/jwtManager');
+const cors = require("cors")
+
+const app = express();
+const port = 8080;
+const users = [];
+
+const secretKey = process.env.SECRET_KEY; 
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials:true,
+}))
+
+// User registration
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  users.push({ username, password });
+  res.status(201).send('User registered');
+});
+
+// User login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (!user) return res.status(401).json('Please Check your details');
+
+  const token = jwt.sign({ username: user.username }, secretKey);
+  res.status(200).json({ token });
+});
+
+// Protected route
+app.get('/protected', authenticateToken, (req, res) => {
+  res.status(200).json(`Hello ${req.user.username}, this is a protected route.`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Home() {
+  const [msg, setMsg] = useState("");
+  const [token, setToken] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("accessKey");
+    if (storedToken) {
+      setToken(storedToken);
+    }else{
+        setError("No token found");
+    }
+  }, [getdata]);
+
+  async function getdata() {
+    try {
+      const respone = await axios.get("http://localhost:8080/protected", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setMsg(respone.data);
+    } catch (err) {
+      setError(err.message);
+    }
+  }
+
+  return (
+    <>
+    <h1>Welcome</h1>
+    <button onClick={getdata}>Check Data</button>
+    {msg && <p>{msg}</p>}
+    </>
+  );
+}
+
+export default Home;
